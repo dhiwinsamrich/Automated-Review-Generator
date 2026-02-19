63d34477d451455ce03d1fd2b306a35586c4d34b
dhiwinsamrich <dhiwinsamrich916@gmail.com>
2026-02-19 17:02:30 +0530

    Added: App.tsx file for the routes - Added Routes path for the additional conditions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ReviewPage from "./pages/ReviewPage";
 import ActionConfirmedPage from "./pages/ActionConfirmedPage";
+import PrivacyPolicyPage from "./pages/PrivacyPolicyPage";
+import TermsConditionsPage from "./pages/TermsConditionsPage";
+import TermsOfServicePage from "./pages/TermsOfServicePage";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +23,9 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/review/:token" element={<ReviewPage />} />
           <Route path="/action-confirmed" element={<ActionConfirmedPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="/terms-and-conditions" element={<TermsConditionsPage />} />
+          <Route path="/terms-of-service" element={<TermsOfServicePage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
